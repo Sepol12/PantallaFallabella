b86c538f0c923543f51999e93cffc5507a9957f6
Sepol12 <crislol126@hotmail.com>
2024-04-30 11:08:21 -0500

    boton lo quiero

--- proximate-apps-landing-falabella-b738d31b3235/src/app/app.component.ts
+++ proximate-apps-landing-falabella-b738d31b3235/src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component, ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-component',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private elementRef: ElementRef) {}
-
-  scrollToSection() {
-    const section = this.elementRef.nativeElement.querySelector('#envianos-tus-datos');
-    if (section) {
-      section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  
+  constructor(private elementRef: ElementRef) { }
+  
+  scrollToDestination() {
+    const destination = this.elementRef.nativeElement.querySelector('#enviarConocerBeneficios');
+    if (destination) {
+      destination.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
   }
 }
